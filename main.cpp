2a657c87d1f0001943a29c534410864976338979
Kyu Lee <kwooler@gmail.com>
2024-03-16 14:22:47 -0700

    M1Q1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-#include <fstream>
-#include <iostream>
-using namespace std;
+#include "main.hpp"
 
 int main()
 {
-	// *************************
-      	// Complete Your Code
-	// *************************
+	double score;
+	string sname;
+	int ret1;
+	double ret2;
+
+	// Call printStudents()
+	score = 95.0;
+	ret1 = printStudents(score);
+	cout << "The total printed students " << ret1 << endl;
+
+	/****************************************************/
+	// Call getAVG()
+	/****************************************************/
+	sname = "Jason";
+	ret2 = getAVG(sname);
+	cout << "The average for " << sname << "\t" << ret2 << endl;
 }
